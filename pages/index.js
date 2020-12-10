6f1dc64c68fbd9692bbfbb8e7c62919c4081f641
cardfaux <cardfaux@tutanota.com>
2020-12-10 15:56:01 -0600

    added the getStaticProps method to fetch data for blogs from sanity

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,39 @@
 import { Row, Col } from 'react-bootstrap';
-import PageLayout from '../components/PageLayout';
-import AuthorIntro from '../components/AuthorIntro';
-import CardItem from '../components/CardItem';
-import CardListItem from '../components/CardListItem';
+import PageLayout from 'components/PageLayout';
+import AuthorIntro from 'components/AuthorIntro';
+import CardItem from 'components/CardItem';
+import CardListItem from 'components/CardListItem';
 
-export default function Home() {
+import { getAllBlogs } from 'lib/api';
+
+export default function Home({ blogs }) {
   return (
     <PageLayout>
       <AuthorIntro />
       <hr />
       <Row className='mb-5'>
-        <Col md='10'>
+        {/* <Col md="10">
           <CardListItem />
-        </Col>
-        <Col md='4'>
-          <CardItem />
-        </Col>
+        </Col> */}
+        {blogs.map((blog) => (
+          <Col key={blog.slug} md='4'>
+            <CardItem title={blog.title} subtitle={blog.subtitle} />
+          </Col>
+        ))}
       </Row>
     </PageLayout>
   );
+}
+
+// This function is called during the build (build time)
+// Provides props to your page
+// It will create static page
+export async function getStaticProps() {
+  console.log('Calling getStaticProps');
+  const blogs = await getAllBlogs();
+  return {
+    props: {
+      blogs,
+    },
+  };
 }
