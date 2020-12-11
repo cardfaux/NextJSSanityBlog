9a9246cdd93b728a0e42ad64b4f876a15f9a2b36
cardfaux <cardfaux@tutanota.com>
2020-12-11 11:28:39 -0600

    added author image and linked it to the blog post document

--- components/CardItem.jsx
+++ components/CardItem.jsx
@@ -1,34 +1,36 @@
 import { Card } from 'react-bootstrap';
 
-const CardItem = ({ title, subtitle, image, date }) => {
+const CardItem = ({ title, subtitle, image, date, author }) => {
   return (
-    <Card className={`fj-card`}>
-      <div className='card-body-wrapper'>
-        <Card.Header className='d-flex flex-row'>
-          <img
-            src='https://via.placeholder.com/250'
-            className='rounded-circle mr-3'
-            height='50px'
-            width='50px'
-            alt='avatar'
-          />
-          <div>
-            <Card.Title className='font-weight-bold mb-1'>
-              Placeholder Author
-            </Card.Title>
-            <Card.Text className='card-date'>{date}</Card.Text>
+    <>
+      <Card className={`fj-card`}>
+        <div className='card-body-wrapper'>
+          <Card.Header className='d-flex flex-row'>
+            <img
+              src={author?.avatar || 'https://via.placeholder.com/150'}
+              className='rounded-circle mr-3'
+              height='50px'
+              width='50px'
+              alt='avatar'
+            />
+            <div>
+              <Card.Title className='font-weight-bold mb-1'>
+                {author?.name}
+              </Card.Title>
+              <Card.Text className='card-date'>{date}</Card.Text>
+            </div>
+          </Card.Header>
+          <div className='view overlay'>
+            <Card.Img src={image} alt='Card image cap' />
           </div>
-        </Card.Header>
-        <div className='view overlay'>
-          <Card.Img src={image} alt='Card image cap' />
+          <Card.Body>
+            <Card.Title className='card-main-title'>{title}</Card.Title>
+            <Card.Text>{subtitle}</Card.Text>
+          </Card.Body>
         </div>
-        <Card.Body>
-          <Card.Title className='card-main-title'>{title}</Card.Title>
-          <Card.Text>{subtitle}</Card.Text>
-        </Card.Body>
-      </div>
-      <a className='card-button'>Read More</a>
-    </Card>
+        <a className='card-button'>Read More</a>
+      </Card>
+    </>
   );
 };
 
