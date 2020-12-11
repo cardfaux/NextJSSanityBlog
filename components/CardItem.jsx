794b5ded6f091a2bbc2aa1bbc6cb4905b6bc8854
cardfaux <cardfaux@tutanota.com>
2020-12-11 10:02:24 -0600

    added new fields to put it in the blogs on the frontend

--- components/CardItem.jsx
+++ components/CardItem.jsx
@@ -1,12 +1,12 @@
 import { Card } from 'react-bootstrap';
 
-const CardItem = (props) => {
+const CardItem = ({ title, subtitle, image, date }) => {
   return (
     <Card className={`fj-card`}>
       <div className='card-body-wrapper'>
         <Card.Header className='d-flex flex-row'>
           <img
-            src={'https://via.placeholder.com/150'}
+            src='https://via.placeholder.com/250'
             className='rounded-circle mr-3'
             height='50px'
             width='50px'
@@ -16,15 +16,15 @@ const CardItem = (props) => {
             <Card.Title className='font-weight-bold mb-1'>
               Placeholder Author
             </Card.Title>
-            <Card.Text className='card-date'>Placeholder Date</Card.Text>
+            <Card.Text className='card-date'>{date}</Card.Text>
           </div>
         </Card.Header>
         <div className='view overlay'>
-          <Card.Img src='https://via.placeholder.com/250' alt='Card image cap' />
+          <Card.Img src={image} alt='Card image cap' />
         </div>
         <Card.Body>
-          <Card.Title className='card-main-title'>{props.title}</Card.Title>
-          <Card.Text>{props.subtitle}</Card.Text>
+          <Card.Title className='card-main-title'>{title}</Card.Title>
+          <Card.Text>{subtitle}</Card.Text>
         </Card.Body>
       </div>
       <a className='card-button'>Read More</a>
--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,12 @@ export default function Home({ blogs }) {
         </Col> */}
         {blogs.map((blog) => (
           <Col key={blog.slug} md='4'>
-            <CardItem title={blog.title} subtitle={blog.subtitle} />
+            <CardItem
+              title={blog.title}
+              subtitle={blog.subtitle}
+              date={blog.date}
+              image={blog.coverImage}
+            />
           </Col>
         ))}
       </Row>
@@ -25,15 +30,13 @@ export default function Home({ blogs }) {
   );
 }
 
-// This function is called during the build (build time)
-// Provides props to your page
-// It will create static page
 export async function getStaticProps() {
-  console.log('Calling getStaticProps');
+  const randomNumber = Math.random();
   const blogs = await getAllBlogs();
   return {
     props: {
       blogs,
+      randomNumber,
     },
   };
 }
