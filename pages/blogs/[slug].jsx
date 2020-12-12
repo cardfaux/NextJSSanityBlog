e24763f4cc133fb9ca343e010cff6c90674e79ff
cardfaux <cardfaux@tutanota.com>
2020-12-12 14:22:11 -0600

    added content from the sanity backend

--- pages/blogs/[slug].jsx
+++ pages/blogs/[slug].jsx
@@ -1,6 +1,6 @@
 import PageLayout from 'components/PageLayout';
 import BlogHeader from 'components/BlogHeader';
-import { getBlogBySlug, getAllBlogs } from 'lib/api';
+import { getBlogBySlug, getAllBlogs, urlFor } from 'lib/api';
 import { Row, Col } from 'react-bootstrap';
 
 import BlogContent from 'components/BlogContent';
@@ -13,7 +13,7 @@ const BlogDetail = ({ blog }) => {
           <BlogHeader
             title={blog.title}
             subtitle={blog.subtitle}
-            coverImage={blog.coverImage}
+            coverImage={urlFor(blog.coverImage).height(600).url()}
             author={blog.author}
             date={blog.date}
           />
