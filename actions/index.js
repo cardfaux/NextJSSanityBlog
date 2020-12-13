22d6a69370c2cd6776ebfa514245624dfdace0ea
cardfaux <cardfaux@tutanota.com>
2020-12-13 11:54:29 -0600

    added code t get last 3 blogs ordered by date

--- actions/index.js
+++ actions/index.js
@@ -2,10 +2,8 @@ import useSWR from 'swr';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
-export const useGetHello = () => {
-  return useSWR('/api/hello', fetcher);
-};
+export const useGetHello = () => useSWR('/api/hello', fetcher);
 
-export const useGetBlogs = () => {
-  return useSWR(`/api/blogs`, fetcher);
+export const useGetBlogs = (initialData) => {
+  return useSWR(`/api/blogs`, fetcher, { initialData });
 };
