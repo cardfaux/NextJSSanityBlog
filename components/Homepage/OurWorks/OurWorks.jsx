db560330a7ea7b27446343be22d967d8c96fed39
James Hagood <cardfaux@tutanota.com>
2022-04-03 13:45:50 -0500

    added swiper as a dependency

--- components/Homepage/OurWorks/OurWorks.jsx
+++ components/Homepage/OurWorks/OurWorks.jsx
@@ -1,35 +1,137 @@
 import { Col } from 'react-bootstrap';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 import NewCardItem from '../NewCardItem';
-import { OurWorksSection } from './OurWorksStyles';
 import PhoenixEgg from '../PhoenixEgg';
+
+import { OurWorksSection } from './OurWorksStyles';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+import { Navigation } from 'swiper';
 
 const OurWorks = ({ projects }) => {
   return (
-    <OurWorksSection className='our--works'>
-      <h1>our works</h1>
-      <div className='works--grid'>
-        {projects.map((project, index) => {
-          return (
-            <Col key={index} md='12'>
-              <NewCardItem
-                slug={project.slug}
-                image={project.coverImage}
-                title={project.projectTitle}
-                subTitle={project.projectSubtitle}
-                link={{
-                  href: '/projects/[slug]',
-                  as: `/projects/${project.slug}`,
-                }}
-              />
-            </Col>
-          );
-        })}
-      </div>
-      <div className='phoenix--egg'>
-        <PhoenixEgg />
-      </div>
-    </OurWorksSection>
+    // <OurWorksSection className='our--works'>
+    //   <h1>our works</h1>
+    //   <div className='works--grid'>
+    //     <Swiper
+    //       navigation={true}
+    //       modules={[Navigation]}
+    //       spaceBetween={0}
+    //       slidesPerView={2}
+    //       loop={true}
+    //       breakpoints={{
+    //         640: {
+    //           slidesPerView: 3,
+    //           spaceBetween: 0,
+    //         },
+    //         980: {
+    //           slidesPerView: 4,
+    //           spaceBetween: 0,
+    //         },
+    //       }}
+    //       pagination={{ clickable: true }}
+    //       onSlideChange={() => console.log('slide change')}
+    //       onSwiper={(swiper) => console.log(swiper)}
+    //     >
+    //       {projects.map((project, index) => {
+    //         return (
+    //           <SwiperSlide key={index}>
+    //             <Col key={index} md='12'>
+    //               <NewCardItem
+    //                 slug={project.slug}
+    //                 image={project.coverImage}
+    //                 title={project.projectTitle}
+    //                 subTitle={project.projectSubtitle}
+    //                 link={{
+    //                   href: '/projects/[slug]',
+    //                   as: `/projects/${project.slug}`,
+    //                 }}
+    //               />
+    //             </Col>
+    //           </SwiperSlide>
+    //         );
+    //       })}
+    //     </Swiper>
+    //   </div>
+    //   <div className='phoenix--egg'>
+    //     <PhoenixEgg />
+    //   </div>
+    // </OurWorksSection>
+
+    // <OurWorksSection className='our--works'>
+    //   <h1>our works</h1>
+    //   <div className='works--grid'>
+    //     {projects.map((project, index) => {
+    //       return (
+    //         <Col key={index} md='12'>
+    //           <NewCardItem
+    //             slug={project.slug}
+    //             image={project.coverImage}
+    //             title={project.projectTitle}
+    //             subTitle={project.projectSubtitle}
+    //             link={{
+    //               href: '/projects/[slug]',
+    //               as: `/projects/${project.slug}`,
+    //             }}
+    //           />
+    //         </Col>
+    //       );
+    //     })}
+    //   </div>
+    //   <div className='phoenix--egg'>
+    //     <PhoenixEgg />
+    //   </div>
+    // </OurWorksSection>
+    <>
+      <OurWorksSection className='our--works'>
+        <h1>our works</h1>
+        <Swiper
+          navigation={true}
+          modules={[Navigation]}
+          spaceBetween={0}
+          slidesPerView={2}
+          loop={true}
+          breakpoints={{
+            640: {
+              slidesPerView: 3,
+              spaceBetween: 0,
+            },
+            980: {
+              slidesPerView: 3,
+              spaceBetween: 0,
+            },
+          }}
+          pagination={{ clickable: true }}
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
+          className='mySwipper'
+        >
+          {projects.map((project, index) => {
+            return (
+              <SwiperSlide key={index}>
+                <Col key={index} md='12'>
+                  <NewCardItem
+                    slug={project.slug}
+                    image={project.coverImage}
+                    title={project.projectTitle}
+                    subTitle={project.projectSubtitle}
+                    link={{
+                      href: '/projects/[slug]',
+                      as: `/projects/${project.slug}`,
+                    }}
+                  />
+                </Col>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+        <div className='phoenix--egg'>
+          <PhoenixEgg />
+        </div>
+      </OurWorksSection>
+    </>
   );
 };
 
--- components/Homepage/OurWorks/OurWorksStyles.js
+++ components/Homepage/OurWorks/OurWorksStyles.js
@@ -22,69 +22,61 @@ export const OurWorksSection = styled.section`
     bottom: 0;
     left: 0;
   }
-  .works--grid {
-    display: grid;
-    /* grid-template-columns: 1fr 1fr 1fr; */
-    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-    align-items: center;
-    justify-items: center;
-    gap: 25px;
-    a {
-      color: #ffffff00;
-    }
-    .works--item {
-      position: relative;
-      width: 100%;
-      aspect-ratio: 1 / 1;
-      /* background-color: #404071; */
-      border: inset;
-      ::after {
-        content: '';
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        width: 100%;
-        height: 100%;
-        opacity: 0.2;
-        background-color: #ffffff;
-      }
-    }
-    .works--item.featured {
-      max-width: 100%;
-      background-color: #3bd0ff;
-      transform: scale(1.06);
-    }
-    .works--text {
-      padding: 30px;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: start;
+  a {
+    color: #ffffff00;
+  }
+  .works--item {
+    position: relative;
+    width: 100%;
+    aspect-ratio: 1 / 1;
+    /* background-color: #404071; */
+    border: inset;
+    ::after {
+      content: '';
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
       width: 100%;
       height: 100%;
-      img {
-        max-width: 100%;
-        height: auto;
-        object-fit: cover;
-        z-index: 2;
-      }
-      h3 {
-        color: #ffffff;
-        font-family: 'Josefin Sans', sans-serif;
-        font-weight: 700;
-        font-size: 33px;
-        text-transform: uppercase;
-        z-index: 2;
-      }
-      p {
-        color: #ffffff;
-        font-family: 'Josefin Sans', sans-serif;
-        font-weight: 400;
-        font-size: 13px;
-        z-index: 2;
-      }
+      opacity: 0.2;
+      background-color: #ffffff;
+    }
+  }
+  .works--item.featured {
+    max-width: 100%;
+    background-color: #3bd0ff;
+    transform: scale(1.06);
+  }
+  .works--text {
+    padding: 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: start;
+    width: 100%;
+    height: 100%;
+    img {
+      max-width: 100%;
+      height: auto;
+      object-fit: cover;
+      z-index: 2;
+    }
+    h3 {
+      color: #ffffff;
+      font-family: 'Josefin Sans', sans-serif;
+      font-weight: 700;
+      font-size: 33px;
+      text-transform: uppercase;
+      z-index: 2;
+    }
+    p {
+      color: #ffffff;
+      font-family: 'Josefin Sans', sans-serif;
+      font-weight: 400;
+      font-size: 13px;
+      z-index: 2;
     }
   }
   .phoenix--egg {
